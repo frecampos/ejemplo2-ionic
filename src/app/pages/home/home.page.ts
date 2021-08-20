c0ff1182a6c9d3ad4bde65f28038347e5ab6527c
frecampos <fmcampos_new@hotmail.com>
2021-08-20 13:03:06 -0400

    confirmo

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  constructor(private router: Router, private navControl: NavController) {}
+
+  irPagina2(){
+    this.router.navigate(['/pagina2']);
+  }  
+
+  irPagina3(){
+    this.navControl.navigateForward(['/pagina3']);
+  }
+}
